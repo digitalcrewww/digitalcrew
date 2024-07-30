517d67197f37a177dfe104382674d483d9813686
Stanislas Kokanosky <stanislas.kokanosky@gmail.com>
2024-07-30 18:15:02 +0200

    Changed cache duration from 1 year to 'forever'

--- app/services/setup_status_service.rb
+++ app/services/setup_status_service.rb
@@ -1,14 +1,13 @@
 class SetupStatusService
   CACHE_KEY = 'app_setup_completed'.freeze
-  CACHE_DURATION = 1.year
 
   def self.completed?
-    Rails.cache.fetch(CACHE_KEY, expires_in: CACHE_DURATION) do
+    Rails.cache.fetch(CACHE_KEY) do
       Setting.exists?
     end
   end
 
   def self.mark_as_completed
-    Rails.cache.write(CACHE_KEY, true, expires_in: CACHE_DURATION)
+    Rails.cache.write(CACHE_KEY, true)
   end
 end
